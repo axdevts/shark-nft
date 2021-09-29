629f66f27c3cda6c7d0ec01f8f51804149646029
javascript-coding-fan <longgeli28@gmail.com>
2021-09-29 07:17:32 -0400

    add comment

--- contracts/SharkNFT.sol
+++ contracts/SharkNFT.sol
@@ -10,12 +10,9 @@ contract SharkNFT is ERC721Enumerable, Ownable {
     using Strings for uint256;
     using ECDSA for bytes32;
 
-    // uint256 public constant SVS_GIFT = 88;
-    // uint256 public constant SVS_PRIVATE = 800;
-    // uint256 public constant SVS_PUBLIC = 8000;
-    uint256 public constant SVS_MAX = 7777;
-    uint256 public SVS_PRICE = 0.07 ether;
-    uint256 public constant SVS_PER_MINT = 5;
+    uint256 public constant BBG_MAX = 7777; // total amount
+    uint256 public BBG_PRICE = 0.07 ether; // price per nft
+    uint256 public constant BBG_PER_MINT = 5; // limit mint
 
     mapping(address => bool) public presalerList;
     mapping(address => uint256) public presalerListPurchases;
@@ -25,9 +22,6 @@ contract SharkNFT is ERC721Enumerable, Ownable {
     string private _contractURI;
     string private _tokenBaseURI =
         "https://opensea-creatures-api.herokuapp.com/api/";
-    // string private _tokenBaseURI = "https://SharkNFT.gg/api/metadata/";
-    // address private _artistAddress = 0xea68212b0450A929B14726b90550933bC12fF813;
-    // address private _signerAddress = 0x7e3999B106E4Ef472E569b772bF7F7647D8F26Ba;
 
     string public proof;
     uint256 public tokenIdAmount;
@@ -41,11 +35,13 @@ contract SharkNFT is ERC721Enumerable, Ownable {
 
     constructor() ERC721("SharkNFT", "BNS") {}
 
+    // metadata methods
     modifier notLocked() {
         require(!locked, "Contract metadata methods are locked");
         _;
     }
 
+    // add to presale list
     function addToPresaleList(address[] calldata entries) external onlyOwner {
         for (uint256 i = 0; i < entries.length; i++) {
             address entry = entries[i];
@@ -56,6 +52,7 @@ contract SharkNFT is ERC721Enumerable, Ownable {
         }
     }
 
+    // remove presale list
     function removeFromPresaleList(address[] calldata entries)
         external
         onlyOwner
@@ -68,6 +65,7 @@ contract SharkNFT is ERC721Enumerable, Ownable {
         }
     }
 
+    // hash transaction
     function hashTransaction(
         address sender,
         uint256 qty,
@@ -83,52 +81,33 @@ contract SharkNFT is ERC721Enumerable, Ownable {
         return hash;
     }
 
-    function buy(
-        // bytes32 hash,
-        // bytes memory signature,
-        // string memory nonce,
-        uint256 tokenQuantity,
-        string[] calldata uris
-    ) external payable {
+    // mining with metadata uri
+    function mint(uint256 tokenQuantity, string[] calldata uris)
+        external
+        payable
+    {
         require(saleLive, "SALE_CLOSED");
         require(!presaleLive, "ONLY_PRESALE");
-        // require(matchAddresSigner(hash, signature), "DIRECT_MINT_DISALLOWED");
-        // require(!_usedNonces[nonce], "HASH_USED");
-        // require(
-        //     hashTransaction(msg.sender, tokenQuantity, nonce) == hash,
-        //     "HASH_FAIL"
-        // );
-        require(totalSupply() < SVS_MAX, "OUT_OF_STOCK");
-        // require(
-        // publicAmountMinted + tokenQuantity <= SVS_PUBLIC,
-        // "EXCEED_PUBLIC"
-        // );
-        require(tokenQuantity <= SVS_PER_MINT, "EXCEED_SVS_PER_MINT");
-        // require(SVS_PRICE * tokenQuantity <= msg.value, "INSUFFICIENT_ETH");
+        require(totalSupply() < BBG_MAX, "OUT_OF_STOCK");
+        require(tokenQuantity <= BBG_PER_MINT, "EXCEED_PER_MINT");
 
         for (uint256 i = 0; i < tokenQuantity; i++) {
             publicAmountMinted++;
             metadataUris[tokenIdAmount] = uris[i];
             _safeMint(msg.sender, totalSupply() + 1);
         }
-
-        // _usedNonces[nonce] = true;
     }
 
     function presaleBuy(uint256 tokenQuantity) external payable {
         require(!saleLive && presaleLive, "PRESALE_CLOSED");
         require(presalerList[msg.sender], "NOT_QUALIFIED");
-        require(totalSupply() < SVS_MAX, "OUT_OF_STOCK");
-        // require(
-        // privateAmountMinted + tokenQuantity <= SVS_PRIVATE,
-        // "EXCEED_PRIVATE"
-        // );
+        require(totalSupply() < BBG_MAX, "OUT_OF_STOCK");
         require(
             presalerListPurchases[msg.sender] + tokenQuantity <=
                 presalePurchaseLimit,
             "EXCEED_ALLOC"
         );
-        require(SVS_PRICE * tokenQuantity <= msg.value, "INSUFFICIENT_ETH");
+        require(BBG_PRICE * tokenQuantity <= msg.value, "INSUFFICIENT_ETH");
 
         for (uint256 i = 0; i < tokenQuantity; i++) {
             privateAmountMinted++;
@@ -137,14 +116,17 @@ contract SharkNFT is ERC721Enumerable, Ownable {
         }
     }
 
+    // get total amount
     function getAmount() external view onlyOwner returns (uint256) {
-        return (SVS_MAX - publicAmountMinted - privateAmountMinted);
+        return (BBG_MAX - publicAmountMinted - privateAmountMinted);
     }
 
+    // set nft price
     function setPrice(uint256 _price) external onlyOwner {
-        SVS_PRICE = _price;
+        BBG_PRICE = _price;
     }
 
+    // update metadata uri
     function updateMetadatauri(uint256 tokenId, string calldata uri)
         external
         onlyOwner
@@ -153,8 +135,8 @@ contract SharkNFT is ERC721Enumerable, Ownable {
     }
 
     function gift(address[] calldata receivers) external onlyOwner {
-        require(totalSupply() + receivers.length <= SVS_MAX, "MAX_MINT");
-        // require(giftedAmount + receivers.length <= SVS_GIFT, "GIFTS_EMPTY");
+        require(totalSupply() + receivers.length <= BBG_MAX, "BBG_MAX_MINT");
+        // require(giftedAmount + receivers.length <= GIFT, "GIFTS_EMPTY");
 
         for (uint256 i = 0; i < receivers.length; i++) {
             giftedAmount++;
