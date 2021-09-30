41ace2d74fc6c8abfffd351888a8921aff346195
javascript-coding-fan <longgeli28@gmail.com>
2021-09-30 15:01:42 -0400

    add comment and some functions

--- contracts/BaboonBrigade.sol
+++ contracts/BaboonBrigade.sol
@@ -14,47 +14,107 @@ contract BaboonBrigade is ERC721Enumerable, Ownable {
     uint256 public BBG_PRICE = 0.07 ether; // price per nft
     uint256 public constant BBG_PER_MINT = 5; // limit mint
 	address payable public companyAddress = payable(0x2d2DA12fFb23e1025d2490657e96289C37FC28B3); // company address
-    mapping(uint256 => string) public metadataUris;
 
-    string private _tokenBaseURI =
-        "https://ipfs.io/ipfs/";
+    string private tokenBaseURI =
+        "https://ipfs.io/ipfs/QmZ2M1bcmsi18n7pY4XR5c2FEyBgMbNFgMJyuYR39kH8a6/";
 
-    uint256 public tokenIdAmount;
+    uint256 public totalMintedAmount;
+
+	event Mint(uint256 tokenId, address sender, uint amount);
+	event PrivateMint(uint256 tokenId, address sender, uint amount);
 
     constructor() ERC721("BaboonBrigade", "BBG") {}
 
-    // mining with metadata uri
-    function mint(uint256 tokenQuantity, string[] calldata uris)
+	/**
+     * @notice Results a finished mint
+     * @dev Only admin or smart contract
+     * @dev Token Id can only be resulted if there has been a bidder and reserve met.
+     * @param tokenQuantity token ID which need to be finished
+    */
+    function mint(uint256 tokenQuantity)
         external
         payable
     {
         require(totalSupply() < BBG_MAX, "OUT_OF_STOCK");
         require(tokenQuantity <= BBG_PER_MINT, "EXCEED_PER_MINT");
+		require(BBG_PRICE * tokenQuantity <= msg.value, "Not Enough payments");
 
         for (uint256 i = 0; i < tokenQuantity; i++) {
-			tokenIdAmount++;
-            metadataUris[tokenIdAmount] = uris[i];
+			totalMintedAmount++;
             _safeMint(msg.sender, totalSupply() + 1);
 			companyAddress.transfer(msg.value);
-			emit mint(tokenIdAmount, uris[i], msg.sender, companyAddress, msg.value);
+			emit Mint(totalMintedAmount, msg.sender, msg.value);
         }
     }
 
-    // get total amount
-    function getAmount() external view returns (uint256) {
-        return (BBG_MAX - tokenIdAmount);
+	/**
+     * @notice Results a finished private mint
+     * @dev Only admin
+     * @dev Token Id can only be resulted if there has been a bidder and reserve met.
+     * @param tokenQuantity token ID which need to be finished
+    */
+	function privateMint(uint256 tokenQuantity)
+        external
+        payable
+		onlyOwner
+    {
+        require(totalSupply() < BBG_MAX, "OUT_OF_STOCK");
+        require(tokenQuantity <= BBG_PER_MINT, "EXCEED_PER_MINT");
+		require(BBG_PRICE * tokenQuantity <= msg.value, "Not Enough payments");
+
+        for (uint256 i = 0; i < tokenQuantity; i++) {
+			totalMintedAmount++;
+            _safeMint(msg.sender, totalSupply() + 1);
+			companyAddress.transfer(msg.value);
+			emit PrivateMint(totalMintedAmount, msg.sender, msg.value);
+        }
     }
 
-    // set nft price
+    /**
+     * @notice Results a Remaining amount
+     * @dev Total amount - minted amount
+    */
+    function getAmount() external view returns (uint256) {
+        return (BBG_MAX - totalMintedAmount);
+    }
+
+    /**
+     * @notice Results a price per NFT
+	 * @dev Only admin
+     * @dev Set price per NFT
+    */
     function setPrice(uint256 _price) external onlyOwner {
         BBG_PRICE = _price;
     }
 
-    // update metadata uri
-    function updateMetadatauri(uint256 tokenId, string calldata uri)
-        external
-        onlyOwner
+	/**
+     * @notice Results a metadata URI
+	 * @param tokenId token URI per token ID
+    */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721)
+        returns (string memory)
     {
-        metadataUris[tokenId] = uri;
+        require(_exists(tokenId), "Cannot query non-existent token");
+
+        return string(abi.encodePacked(tokenBaseURI, tokenId.toString()));
     }
+
+	/**
+     * @notice Results a metadata uri
+     * @param _tokenBaseURI token ID which need to be finished
+    */
+	function setTokenBaseUri(string _tokenBaseURI) public {
+		tokenBaseURI = _tokenBaseURI;
+	}
+
+	/**
+     * @notice Results a company wallet address
+     * @param addr change another wallet address from wallet address
+    */
+	function setCompanyAddress(address addr) public onlyOwner {
+		companyAddress = addr;
+	}
 }
